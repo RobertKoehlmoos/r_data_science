9ec8e4e0e6e74e18ea3c122c6f10442cb5e7e14a
rob <robert.koehlmoos@gmail.com>
2021-08-04 22:21:08 -0400

    Learned about rmarkdown

--- many_models.R
+++ many_models.R
@@ -23,3 +23,35 @@ add_residuals(nz, nz_mod) |>
   geom_hline(yintercept = 0, color = "white", size = 3) +
   geom_line() +
   ggtitle("Remaining patter")
+
+by_country <- gapminder |>
+  group_by(country, continent) |>
+  nest()
+
+country_model <- function(df) {
+  lm(lifeExp ~ year, data = df)
+}
+
+by_country <- mutate(by_country, model =  map(data, country_model))
+
+by_country <- mutate(by_country, residuals = map2(data, model, add_residuals))
+
+resids <- unnest(by_country, residuals)
+
+resids |>
+  ggplot(aes(year, resid)) +
+  geom_line(aes(group = country), alpha = 1/2) +
+  geom_smooth(se = FALSE)
+
+resids |>
+  ggplot(aes(year, resid, group = country)) +
+  geom_line(alpha = 0.3) +
+  facet_wrap(~continent)
+
+
+# looking at model quality ------------------------------------------------
+
+broom::glance(nz_mod)  
+glanced <- by_country |>
+  mutate(glance = map(model, broom::glance)) |>
+  unnest(glance, .drop = TRUE)

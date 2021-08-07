9420c440a82b5e80555c903c48bbb38380c2fb9e
rob <robert.koehlmoos@gmail.com>
2021-08-06 21:36:53 -0400

    Done with R! Until I need it

--- graphics_communication.R
+++ graphics_communication.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 
 
-
 # Graphics to communicate -------------------------------------------------
 ggplot(mpg, aes(displ, hwy)) +
   geom_point(aes(color = class)) +
@@ -28,3 +27,36 @@ ggplot(df, aes(x, y)) +
     x = quote(sum(x[i]^2, i == 1, n)),
     y = quote(alpha + beta + frac(delta, theta))
   )
+
+
+# practice -----------------------------------------------------------------
+
+ggplot(mpg, aes(cty, hwy)) +
+  geom_point(aes(color = trans)) +
+  labs(
+    title = "My cool graphixxxs!",
+    x = "year",
+    y = "miles per gatorade",
+    subtitle = "Please hire me",
+    color = "Is it color or colour?"
+  )
+
+best_in_class <- mpg |>
+  group_by(class) |>
+  filter(row_number(desc(hwy)) == 1)
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_point(size = 3, shape = 1, data = best_in_class) +
+  ggrepel::geom_label_repel(
+    aes(label = model),
+    data = best_in_class
+  )
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point() +
+  scale_x_continuous(labels = NULL)
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  theme_dark()
